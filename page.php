048308c58aaf25ead2efbd5ee915def7575fb132
GabrielDev <gab@gab.dev>
2022-03-22 03:02:13 -0400

    correction d'instructions de syntaxe php

--- page.php
+++ page.php
@@ -3,7 +3,7 @@
 
    <?php if (have_posts()): the_post(); ?>
         <?php the_title(); ?>
-        <?php the_content() ?>        
+        <?php the_content(); ?>
    <?php endif ?>
 </main>
 <?php get_footer(); ?>
