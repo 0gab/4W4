8f99d391d677e127dda2a29d9eea87dd75b4dbd6
GabrielDev <gab@gab.dev>
2022-03-22 02:51:11 -0400

    Upload initial des fichiers

---
+++ Devoyau-L_Gabriel_4W4_TP1/search.php
@@ -0,0 +1,14 @@
+<?php get_header(); ?>
+<main class="site-main">
+    <?php if(have_posts()): ?>
+
+    <?php while(have_posts()): the_post(); ?>
+    <h3><?php echo get_permalink(); ?></h3>
+    <?php the_title(); ?>
+    <p><?php echo wp_trim_words(get_the_content(), 20, "...>"); ?></p>
+    <hr>
+    <?php endwhile; ?>
+    <?php else : ?><p>Cette recherche n'a pas retournée de résultat</p>
+    <?php endif; ?>
+</main>
+<?php get_footer(); ?>
