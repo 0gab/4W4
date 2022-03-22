ede96ae9f4a377f519f28be5f0cae54d7e5c33bc
GabrielDev <gab@gab.dev>
2022-03-22 03:01:09 -0400

    correction d'instructions de syntaxe php

--- front-page.php
+++ front-page.php
@@ -12,7 +12,7 @@
     <h1>Accueil</h1>
    <?php if (have_posts()): the_post(); ?>
         <?php the_title(); ?>
-        <?php the_content() ?>   
+        <?php the_content(); ?>
   
    <?php endif ?>
    
